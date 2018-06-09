dc0486246644f54291f641e9157f09e94b3f8726
saugandhkaran <saugandhkaran@gmail.com>
2018-06-09 23:55:19 +0200

    security checking

--- public/pages/confirmation/confirmation.js
+++ public/pages/confirmation/confirmation.js
@@ -1,7 +1,27 @@
 angular.module('app.confirmationController',[])
-    .controller('confirmationController',['$scope','$timeout','$location', function($scope, $timeout, $location){
-
+    .controller('confirmationController',['$scope','$timeout','$location', '$http','$window', function($scope, $timeout, $location, $http, $window){
+    
+    $scope.error = false;
     $scope.processed = false;
+    var token = $window.sessionStorage.getItem("token");
+    var config = {
+        headers : {'Content-Type': 'application/json',
+        'Authorization' : 'DirectLogin token=\"' + token + '\"'
+      }}
+
+    $http.get('https://beyondbanking.openbankproject.com/obp/v3.0.0/users/current',config).then(
+        function(success){
+            var userId = success.data.token;
+            $http.get('https://webapisecuredbb.azurewebsites.net/user/:token/documents', userId).then(function(success){
+                $scope.docs = success.data;
+            }, function(error){
+                $scope.error = true;
+            })
+
+        }, function(error){
+            $scope.error = true;
+        }
+    );
 
     $scope.approve = function(){
         $scope.processed = true;
@@ -9,7 +29,6 @@ angular.module('app.confirmationController',[])
         $timeout( function(){
             $location.path('/success');
         }, 2000 );
-
     }
 
 }]);
--- public/pages/login/login.js
+++ public/pages/login/login.js
@@ -1,8 +1,23 @@
 angular.module('app.loginController',[])
-    .controller('loginController',['$scope','$timeout','$location', function($scope, $timeout, $location){
+    .controller('loginController',['$scope','$timeout','$location', '$http','$window', function($scope, $timeout, $location, $http, $window){
 
-    $scope.goToConfirmation = function(){
-        $location.path('/confirmation');
+
+    $scope.loginObj = {};
+    
+    $scope.login = function(){
+
+        var config = {
+            headers : {'Content-Type': 'application/json',
+            'Authorization' : 'DirectLogin username=\"'+$scope.loginObj.username +'\",password=\"'+ $scope.loginObj.password +'\",consumer_key="ukj43wr0oc5ivqlvmbbawvyzkp0vo1vx3fcyal1n"'
+          }}
+        
+          var data = {};
+
+        $http.post('https://beyondbanking.openbankproject.com/my/logins/direct', data, config)
+          .then(function(response){
+            $window.sessionStorage.setItem("token", response.data.token);
+            $location.path('/confirmation');
+          });
     }
 
-}]);
+    }]);
